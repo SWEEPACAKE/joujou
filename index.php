8ada8c5ec2cd2c42bace32f34f22015d4f079c05
TimTomUser <4mtosengo@gmail.com>
2025-10-30 11:46:40 +0100

    #

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-// Bonjour, ici on va s'amuser à faire n'importe quoi
-// Salut c'est Nico
-// Salut c'est Thomas
-// Salut c'est Carole
-//Bondour c'est Dylan
-// Salut c'est Hugo Pinauu
-// Bonjour tout le monde, c'est Abdel
-// Bonjour tout le monde, c'est Manu
-// Bonjour de Nadège
+// Hello World if a neophyte functional code 
+// 
