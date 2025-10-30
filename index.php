068dc4ed2d1515336401eeb4ab5d86b4199c1829
Astrophaze <hugo.astro03@gmail.com>
2025-10-30 11:58:54 +0100

    Hugo

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 // Salut c'est Nico
 // Salut c'est Thomas
 // Salut c'est Carole
-//Bondour c'est Dylan
-// Salut c'est Hugo Pinauu
-// Bonjour tout le monde, c'est Abdel
-// Bonjour tout le monde, c'est Manu
-// Bonjour de Nadège
+?>
+<h1 class="titre">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis similique vel porro unde ad possimus nam ipsam neque facilis consequuntur in pariatur sint aliquam, quae qui. Veritatis nam facilis fugit illo similique optio repudiandae laborum obcaecati dicta! Similique assumenda dolorum eaque eos, quisquam totam voluptate minus reiciendis. Consectetur id officia ratione repudiandae debitis voluptatibus necessitatibus nobis reiciendis. Molestiae harum ratione iure mollitia libero fugit labore numquam, doloremque nesciunt ut explicabo impedit ea recusandae alias ducimus iste necessitatibus deleniti officiis officia facilis quam nulla exercitationem quo laudantium? Sequi laudantium excepturi porro aspernatur fugiat ullam reprehenderit perspiciatis illo suscipit illum tempora incidunt quod pariatur cupiditate nihil laborum, consequuntur voluptates libero veritatis! Quae blanditiis vel at aut architecto consequuntur error unde id voluptates voluptas. Cumque nostrum aliquam repellat molestias quia labore sequi accusamus rem. Saepe fugit ducimus animi totam neque ea laborum at accusantium temporibus sit, numquam illum nisi dolorem aperiam nobis facere?</h1>
+<p class="contenu">contenu trop cool</p>
+<!-- lorem9999999 -->
+ <!-- Mon sac est fait -->
