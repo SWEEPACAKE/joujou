7b375190bd9d7f25308a96b3c37c75473bbb9aab
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2025-10-30 11:47:16 +0100

    Test

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 // Salut c'est Thomas
 // Salut c'est Carole
 //Bondour c'est Dylan
-// Salut c'est Hugo Pinauu
+// Salut c'est Hugo Pineau
 // Bonjour tout le monde, c'est Abdel
 // Bonjour tout le monde, c'est Manu
 // Bonjour de Nadège
+?>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ipsa eligendi! Similique autem aut blanditiis neque enim, obcaecati modi ratione, suscipit, eveniet officia minus corporis consequatur perferendis veritatis repellat maxime reprehenderit. Iure sit eius cupiditate dolore neque dolorum obcaecati itaque quas nemo, quae dolorem repudiandae. Suscipit quia sequi, sint fugiat officia vel non nobis atque necessitatibus earum, eius assumenda. Beatae voluptatum quod voluptate nisi doloremque. Quas accusantium reiciendis quam quasi a, excepturi quod explicabo quidem recusandae commodi nobis et ipsa provident dolor quibusdam minus deleniti cum similique voluptatibus, numquam enim exercitationem praesentium adipisci? Minima corporis officia praesentium et est exercitationem nesciunt nostrum doloremque! Dolorum velit enim alias beatae natus tenetur vitae animi, possimus veniam minus pariatur dolore explicabo exercitationem! Error perspiciatis praesentium, fugiat quisquam, ut quia itaque voluptatem quibusdam in quasi aliquid voluptates dolorum modi? Culpa corrupti explicabo distinctio maxime. Consectetur fugit velit porro, quam consequuntur at sed nesciunt sunt expedita iure excepturi voluptas numquam iste. Natus laboriosam a aperiam porro inventore! Minus omnis, odio expedita quam dolorem vitae laboriosam saepe aliquid error nesciunt aspernatur soluta voluptatum natus dolorum esse maiores unde commodi illo nam sunt inventore molestias dicta voluptas repudiandae! Minus autem eius soluta quasi dolor assumenda, delectus minima.
