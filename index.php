54507d6196961616b54300d9411e7caa13d1c485
devkarore <karore.cg@gmail.com>
2025-10-30 11:54:39 +0100

    Bouuuuuuuuuuuuuu

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-// Bonjour, ici on va s'amuser à faire n'importe quoi
-// Salut c'est Nico
-// Salut c'est Thomas
-// Salut c'est Carole
-//Bondour c'est Dylan
-// Salut c'est Hugo Pinauu
-// Bonjour tout le monde, c'est Abdel
-// Bonjour tout le monde, c'est Manu
-// Bonjour de Nadège
+// Boouu !!
